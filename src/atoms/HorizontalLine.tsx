2b433194c10bf61197c3e7f474c28d8c0e29e989
Alex Anson <1alex.anson@gmail.com>
2023-03-03 18:14:45 -0500

    add header and max-width-wrapper :sparkles:

---
+++ src/atoms/HorizontalLine.tsx
@@ -0,0 +1,5 @@
+import styles from "@/atoms/HorizontalLine.module.css";
+
+export function HorizontalLine() {
+  return <hr className={styles.horizontal_line} />;
+}
---
+++ src/components/maxWidthWrapperSection.tsx
@@ -0,0 +1,26 @@
+import styles from "@/components/maxWidthWrapperSection.module.css";
+
+export function MaxWidthWrapperSection() {
+  return (
+    <section>
+      <h2>max-width-wrapper</h2>
+      <div className={styles.maxWidthWrapper}>
+        <div className={styles.maxWidthContent}>
+          <h4>Fulfills these constraints:</h4>
+          <ul>
+            <li>It fills the available space on smaller viewports.</li>
+            <li>
+              It sets a maximum width, and will horizontally center itself
+              within the parent if there is leftover space.
+            </li>
+            <li>
+              It includes a bit of horizontal &quot;breathing room&quot;, so
+              that its children aren&rsquo;t pressed right up against the edges
+              of the viewport.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
