bdac1278fe68e55c1f96d24b659184a94af07a29
Alex Anson <1alex.anson@gmail.com>
2023-03-06 18:10:56 -0500

    refactor price cards

--- src/components/SynergisticIncExample.tsx
+++ src/components/SynergisticIncExample.tsx
@@ -1,47 +1,73 @@
 import styles from "@/components/SynergisticIncExample.module.css";
 
+const starterFeatures = [
+  "3 projects",
+  "100gb monthly transfer",
+  "20gb storage",
+  "No support",
+];
+const proFeatures = [
+  "10 projects",
+  "500gb monthly transfer",
+  "1tb storage",
+  "Email customer support",
+];
+const enterpriseFeatures = [
+  "Unlimited projects, transfers, storage.",
+  "Dedicated phone & email support",
+];
+
 export function SynergisticIncExample(): JSX.Element {
   return (
-    <section className={styles.pricing}>
-      <article className={styles.card}>
-        <h2>Starter</h2>
-        <div className={styles.price}>$29</div>
-        <p>For personal projects.</p>
-        <ul>
-          <li>3 projects</li>
-          <li>100gb monthly transfer</li>
-          <li>20gb storage</li>
-          <li>No support</li>
-        </ul>
-        <button>Buy Now</button>
-      </article>
-
-      <article className={`${styles.primary} ${styles.card}`}>
-        <h2>Pro</h2>
-        <div className={styles.price}>$99</div>
-        <p>
-          For small businesses looking to grow. Our most popular plan for
-          ambitious organizations.
-        </p>
-        <ul>
-          <li>10 projects</li>
-          <li>500gb monthly transfer</li>
-          <li>1tb storage</li>
-          <li>Email customer support</li>
-        </ul>
-        <button>Buy Now</button>
-      </article>
-
-      <article className={styles.card}>
-        <h2>Enterprise</h2>
-        <div className={styles.price}>Contact Us</div>
-        <p>For large orgs.</p>
-        <ul>
-          <li>Unlimited projects, transers, storage.</li>
-          <li>Dedicated phone & email support</li>
-        </ul>
-        <button>Buy Now</button>
-      </article>
+    <section style={{ marginTop: "30px" }}>
+      <h2>price cards, center card emphasized</h2>
+      <div className={styles.pricing}>
+        <PriceCard
+          heading="Starter"
+          price="$29"
+          description="For personal projects."
+          features={starterFeatures}
+        />
+        <PriceCard
+          heading="Pro"
+          price="$99"
+          description="For small businesses looking to grow. Our most popular plan for
+          ambitious organizations."
+          features={proFeatures}
+          isPrimaryCard
+        />
+        <PriceCard
+          heading="Enterprise"
+          price="Contact Us"
+          description="For large organizations."
+          features={enterpriseFeatures}
+        />
+      </div>
     </section>
+  );
+}
+
+interface PropsPriceCard {
+  heading: string;
+  price: string;
+  description: string;
+  features: string[];
+  isPrimaryCard?: true;
+}
+function PriceCard(props: PropsPriceCard): JSX.Element {
+  return (
+    <article
+      className={`${styles.card} ${props.isPrimaryCard ? styles.primary : ""}`}
+    >
+      <h3>{props.heading}</h3>
+      <div className={styles.price}>{props.price}</div>
+      <p>{props.description}</p>
+      <ul>
+        {props.features.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <button>Buy Now</button>
+    </article>
   );
 }
