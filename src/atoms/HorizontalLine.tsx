82506f51bf955aec55e371e509e4fb532d0dd45b
Alex Anson <1alex.anson@gmail.com>
2023-04-08 20:39:06 -0400

    allow horizontal line to accept styles

--- src/atoms/HorizontalLine.tsx
+++ src/atoms/HorizontalLine.tsx
@@ -1,5 +1,7 @@
 import styles from "@/atoms/HorizontalLine.module.css";
 
-export function HorizontalLine() {
-  return <hr className={styles.horizontal_line} />;
+export function HorizontalLine(props: {
+  styles?: Record<string, string | number>;
+}) {
+  return <hr className={styles.horizontal_line} style={{ ...props.styles }} />;
 }
