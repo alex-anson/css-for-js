85558035596f26c6e8fa2eae5d0525aa48af719e
Alex Anson <1alex.anson@gmail.com>
2023-04-08 20:37:42 -0400

    icon component

---
+++ src/compositions/workshopMod3-miniComponentLib/Icon/Icon.tsx
@@ -0,0 +1,45 @@
+// Libs
+import styled from "styled-components";
+import { Search, AtSign, ChevronDown, Icon } from "react-feather";
+
+const icons: Record<string, Icon> = {
+  search: Search,
+  "at-sign": AtSign,
+  "chevron-down": ChevronDown,
+} as const;
+
+interface Props {
+  id: "search" | "at-sign" | "chevron-down";
+  /** provide unit in `rem` */
+  size: number;
+  /** provide unit in `em` */
+  strokeWidth: number;
+  // FIXME: get `any` out
+  delegated: any;
+}
+
+export default function Icon(props: Props): JSX.Element {
+  const Icon = icons[props.id];
+
+  return (
+    <Wrapper
+      style={{
+        "--size": props.size + "rem",
+        "--stroke-width": props.strokeWidth + "em",
+      }}
+      {...props.delegated}
+    >
+      <Icon color="currentColor" size={props.size} />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: var(--size);
+  height: var(--size);
+
+  & > svg {
+    display: block;
+    stroke-width: var(--stroke-width);
+  }
+`;
